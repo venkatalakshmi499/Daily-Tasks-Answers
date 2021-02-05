18496fed44c774d2629a30a6afb441707eb52f2a
venkatalakshmi499 <68954240+venkatalakshmi499@users.noreply.github.com>
2021-02-05 10:48:48 +0530

    project stage1 angular ecommerce

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ListOrderComponent } from './orders/list-order/list-order.component';
+
+const routes: Routes = [{ path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+{path:'orders',component:ListOrderComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
